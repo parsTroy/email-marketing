7daf8a10197d7c1ff10bbfe0a6c340270a6837fa
parsTroy <troyparsons@hotmail.com>
2025-03-06 14:38:12 -0500

    Update url

--- components/auth/sign-up-form.tsx
+++ components/auth/sign-up-form.tsx
@@ -52,7 +52,7 @@ export function SignUpForm() {
         email: values.email,
         password: values.password,
         options: {
-          emailRedirectTo: `${window.location.origin}/auth/callback`,
+          emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
         },
       });
 
